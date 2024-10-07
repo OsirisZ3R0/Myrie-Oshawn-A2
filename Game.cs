184d3c3c4c5b6656740903e8fb70681cbfb8304b
Oshawn Myrie <teeshawnprime@LIVE.CA>
2024-10-06 23:56:30 -0400

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,49 @@
+﻿// Include code libraries you need below (use the namespace).
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace Game10003
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+       
+
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            Window.SetTitle("Shiney Diamond");
+            Window.SetSize(800, 600);
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+            Window.ClearBackground(Color.OffWhite);
+
+            // Draw fill color
+            Draw.FillColor = Color.Blue;
+            Draw.LineColor = Color.Black;
+            Draw.Line(400, 200, 500, 200);
+            Draw.Line(500, 200, 550, 250);
+            Draw.Line(550, 250, 400, 350);
+            Draw.Line(400, 350, 250, 250);
+            Draw.Line(250, 250, 300, 200);
+            Draw.Line(300, 200, 400, 200);
+
+
+
+
+
+        }
+    } 
+}
